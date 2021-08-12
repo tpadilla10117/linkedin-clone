0ae57c32133e3a3f3271d4ba9be37e57e806c20d
Trinidad Padilla <tpadilla101@live.com>
2021-08-11 18:44:10 -0700

    finished the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 import { HeaderOption } from '../utils';
 
@@ -30,6 +33,10 @@ function Header() {
 
                 <HeaderOption Icon={HomeIcon} title="Home" />
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" /> 
+                <HeaderOption avatar="https://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg" title="Sonny"/> 
 
             </div>
 
--- src/components/HeaderOption.js
+++ src/components/HeaderOption.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './HeaderOption.css';
+import { Avatar } from "@material-ui/core";
 
-function HeaderOption( {title, Icon} ) {
+function HeaderOption( {title, Icon, avatar} ) {
 
     return (
         <div className="headerOption">
 
             { Icon && <Icon className="headerOption__icon" />}
+
+            {avatar && (
+                <Avatar className="headerOption__icon" src={avatar} />
+            )}
 
             <h3 className="headerOption__title">{ title }</h3>
 
